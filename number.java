122cd8d051a28db6d1262744ec54f0a2507c285e
Santhwana P <santhwanap279@gmail.com>
2026-02-20 14:05:05 +0000

    Add files via upload

---
+++ number.java
@@ -0,0 +1,13 @@
+public class number {
+    public static void main(String[] args) {
+
+        int a = 20;
+
+        if (a % 2 == 0) {
+            System.out.println("a is even");
+        } else {
+            System.out.println("a is odd");
+        }
+
+    }
+}
