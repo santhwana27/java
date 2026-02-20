087cad9e76814bd0803714495931e9c3a5614849
Santhwana P <santhwanap279@gmail.com>
2026-02-20 14:15:49 +0000

    Add files via upload

---
+++ Swaptwonumber.java
@@ -0,0 +1,13 @@
+public class Swaptwonumber
+{
+    public static void main(String args[])
+    {
+        int a = 50, b = 80, c;
+
+        c = a;   
+        a = b;   
+        b = c;   
+
+        System.out.println("Swap: a = " + a + " b = " + b);
+    }
+}
