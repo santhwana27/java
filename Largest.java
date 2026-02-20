4eec1e26e40b920650249bbb45ecf7a093442402
Santhwana P <santhwanap279@gmail.com>
2026-02-20 13:52:48 +0000

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,19 @@
+public class Largest
+{
+public static void main(String args[])
+{
+int a=12,b=24,c=9;
+if(a>=b&&a>c)
+{
+System.out.println("Largest:"+a);
+}
+else if(b>=a&&b>=c)
+{
+System.out.println("Largest:"+b);
+}
+else
+{
+System.out.println("Largest:"+c);
+}
+}
+}
