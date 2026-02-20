90ccc262c02e652439bbf70aecbee342f7fdd1e5
Santhwana P <santhwanap279@gmail.com>
2026-02-20 14:05:26 +0000

    Add files via upload

---
+++ SwapWithoutVariable.java
@@ -0,0 +1,16 @@
+public class SwapWithoutVariable {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
