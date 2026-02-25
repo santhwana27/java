744cd67591fbfc2d35c8fbc313266e50a59fd601
Santhwana P <santhwanap279@gmail.com>
2026-02-25 14:19:28 +0000

    Add files via upload

---
+++ typeconversion.java
@@ -0,0 +1,14 @@
+
+public class typeconversion
+{
+public static void main(String args[])
+{
+int a=10;
+float b=a;
+System.out.println("implicit conversion int to float"+b);
+
+double x=132.45678901234567;
+float y=(float)x;
+System.out.println("explicit conversion double to float"+y);
+}
+}
